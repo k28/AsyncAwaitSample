4c65cf7de6bf9240c2e274b173d0d0c6bcf7e6cc
Kazuya Hatano <k28@me.com>
2022-04-02 17:18:48 +0900

    Initial Commit

---
+++ AsyncAwaitSample/AsyncAwaitSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncAwaitSampleApp.swift
+//  AsyncAwaitSample
+//
+//  Created by K.Hatano on 2022/04/02.
+//
+
+import SwiftUI
+
+@main
+struct AsyncAwaitSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainTopView()
+        }
+    }
+}
---
+++ AsyncAwaitSample/View/MainTopView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AsyncAwaitSample
+//
+//  Created by K.Hatano on 2022/04/02.
+//
+
+import SwiftUI
+
+struct MainTopView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainTopView()
+    }
+}
